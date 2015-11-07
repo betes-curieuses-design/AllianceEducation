12b21d7a2849460742fd77e53e28154b77d74cc9
lorajc <lorajc@hotmail.com>
2015-11-07 08:55:04 -0500

    Slider installed

--- plugins/betescurieuses/ae110815/Plugin.php
+++ plugins/betescurieuses/ae110815/Plugin.php
@@ -2,6 +2,9 @@
 
 use System\Classes\PluginBase;
 use Backend;
+use Radiantweb\Problog\Controllers\Posts;
+use Radiantweb\Problog\Models\Post as BasePost;
+use Event;
 
 /**
  * Ae110815 Plugin Information File
@@ -17,10 +20,10 @@ class Plugin extends PluginBase
     public function pluginDetails()
     {
         return [
-            'name'        => 'Ae110815',
+            'name' => 'Ae110815',
             'description' => 'Alliance Education Plugin',
-            'author'      => 'BetesCurieuses',
-            'icon'        => 'icon-graduation-cap'
+            'author' => 'BetesCurieuses',
+            'icon' => 'icon-graduation-cap'
         ];
     }
 
@@ -28,34 +31,57 @@ class Plugin extends PluginBase
     {
         return [
             'aedu' => [
-                'label'       => 'Contenu',
-                'url'         => Backend::url('BetesCurieuses/Ae110815/conferences'),
-                'icon'        => 'icon-graduation-cap',
+                'label' => 'Contenu',
+                'url' => Backend::url('BetesCurieuses/Ae110815/conferences'),
+                'icon' => 'icon-graduation-cap',
                 'permissions' => ['rainlab.blog.*'],
-                'order'       => 500,
+                'order' => 500,
 
                 'sideMenu' => [
                     'conferences' => [
-                        'label'       => 'Conférences',
-                        'icon'        => 'icon-eye',
-                        'url'         => Backend::url('BetesCurieuses/Ae110815/conferences'),
+                        'label' => 'Conférences',
+                        'icon' => 'icon-eye',
+                        'url' => Backend::url('BetesCurieuses/Ae110815/conferences'),
                         'permissions' => ['betescurieuses.ae110815.conferences']
                     ],
                     'formations' => [
-                        'label'       => 'Formations',
-                        'icon'        => 'icon-graduation-cap',
-                        'url'         => Backend::url('BetesCurieuses/Ae110815/formations'),
+                        'label' => 'Formations',
+                        'icon' => 'icon-graduation-cap',
+                        'url' => Backend::url('BetesCurieuses/Ae110815/formations'),
                         'permissions' => ['betescurieuses.ae110815.formations']
                     ],
                     'capsules' => [
-                        'label'       => 'Capsules Vidéo',
-                        'icon'        => 'icon-play-circle-o',
-                        'url'         => Backend::url('BetesCurieuses/Ae110815/capsules'),
+                        'label' => 'Capsules Vidéo',
+                        'icon' => 'icon-play-circle-o',
+                        'url' => Backend::url('BetesCurieuses/Ae110815/capsules'),
                         'permissions' => ['betescurieuses.ae110815.capsules']
                     ]
                 ]
             ]
         ];
+    }
+
+    public function boot()
+    {
+
+        Posts::extendFormFields(function ($form, $model, $context) {
+
+            if (!$model instanceof BasePost)
+                return;
+
+            $form->addTabFields([
+                    'date_event' => [
+                        'label' => 'Date',
+                        'type' => 'datepicker',
+                        'tab' => 'radiantweb.problog::lang.backend.post.fields.tab.general',
+                        'span' => 'left'
+                    ],
+                ]
+            );
+
+
+        });
+
     }
 
 }
---
+++ plugins/radiantweb/problog/controllers/Tags.php
@@ -0,0 +1,24 @@
+<?php namespace Radiantweb\Problog\Controllers;
+
+use BackendMenu;
+use Backend\Classes\Controller;
+
+class Tags extends Controller
+{
+    public $requiredPermissions = ['radiantweb.problog.access_problog_posts'];
+
+    public $implement = [
+        'Backend.Behaviors.FormController',
+        'Backend.Behaviors.ListController'
+    ];
+
+    public $formConfig = 'config_form.yaml';
+    public $listConfig = 'config_list.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        BackendMenu::setContext('Radiantweb.Problog', 'problog', 'tags');
+    }
+}
